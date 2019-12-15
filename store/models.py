fcdeb21124618afe43fb6c7daa4d4e0a9a5f77a0
youpiyoful <yoanfornari@gmail.com>
2019-12-16 00:19:43 +0100

    [wip]create models for store application

--- store/models.py
+++ store/models.py
@@ -1,14 +1,26 @@
 from django.db import models
 
 # Create your models here.
-ARTISTS = {
-    'francis-cabrel': {'name': 'Francis Cabrel'},
-    'lej': {'name': 'Elijay'},
-    'Rosana': {'name': 'Rosana'}
-}
+class Artist(models.Model):
+    name = models.CharField(max_length=200, unique=True)
 
-ALBUMS = [
-    {'name': 'Sarbacane', 'artists': [ARTISTS['francis-cabrel']]},
-    {'name': 'La Dalle', 'artists': [ARTISTS['lej']]},
-    {'name': 'Luna Nueva', 'artists': [ARTISTS['Rosana']]}
-]
+
+class Contact(models.Model):
+    email = models.EmailField(max_length=100)
+    name = models.CharField(max_length=200)
+
+
+class Album(models.Model):
+    reference = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    available = models.BooleanField(default=True)
+    title = models.CharField(max_length=200)
+    picture = models.URLField()
+    artists = models.ManyToManyField(Artist, related_name='albums', blank=True)
+
+
+class Booking(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    contacted = models.BooleanField(default=False)
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    album = models.OneToOneField(Album)
