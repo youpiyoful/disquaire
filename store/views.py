9f956b93f32e71714c626638be24d7bce8ff7069
youpiyoful <yoanfornari@gmail.com>
2020-01-17 03:44:44 +0100

    mise en place des différentes pages html de l'application store

--- store/views.py
+++ store/views.py
@@ -1,8 +1,7 @@
 # from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Album, Artist, Contact, Booking
-from django.template import loader
 
 
 def index(request):
@@ -12,46 +11,48 @@ def index(request):
     :return:
     """
     albums = Album.objects.filter(available=True).order_by('-created_at')[:12]
-    formatted_albums = ["<li>{}</li>".format(album.title) for album in albums]
-    # message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
-    template = loader.get_template('store/index.html')
-    return HttpResponse(template.render(request=request))
+    context = {
+        'albums': albums
+    }
+    return render(request, 'store/index.html', context)
 
 
 def listing(request):
     albums = Album.objects.filter(available=True)
-    formatted_albums = ["<li>{}</li>".format(album['name']) for album in albums]
-    message = """<ul>{}</ul>""".format("\n".join(formatted_albums))
-    return HttpResponse(message)
+    context = {
+        'albums': albums
+    }
+    return render(request, 'store/listing.html', context)
 
 
 def detail(request, album_id):
     id = int(album_id)
     album = Album.objects.get(pk=id)
-    artists = " ".join([artist.name for artist in album.artists.all()])
+    artists = [artist.name for artist in album.artists.all()]
+    artists_name = " ".join(artists)
     message = "Le nom de l'album est {}. Il a été écrit par {}".format(album.title, artists)
-    return HttpResponse(message)
+    context = {
+        'album_title': album.title,
+        'artists_name': artists_name,
+        'album_id': album.id,
+        'thumbnail': album.picture
+    }
+
+    return render(request, 'store/detail.html', context)
 
 
 def search(request):
     query = request.GET.get('query')
     if not query:
-        message = "Aucun artiste n'est demandé"
+        albums = Album.objects.all()
     else:
+        # title contains the query is and query is not sensitive to case.
         albums = Album.objects.filter(title__icontains=query)
-
-        if not albums.exists():
-            albums = Album.objects.filter(artists__name__icontains=query)
-
-        if not albums.exists():
-            message = "Misère de misère, nous n'avons trouvé aucun résultat !"
-        else:
-            albums = ["<li>{}</li>".format(album.title) for album in albums]
-            message = """
-                Nous avons trouvé les albums correspondant à votre requête ! Les voici :
-                <ul>
-                    {}
-                </ul>
-            """.format("<li></li>".join(albums))
-
-    return HttpResponse(message)
+    if not albums.exists():
+        albums = Album.objects.filter(artists__name__icontains=query)
+    title = "Résultats pour la requête %s"%query
+    context = {
+        'albums': albums,
+        'title': title
+    }
+    return render(request, 'store/search.html', context)
