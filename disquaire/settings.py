1f7d8e3be64dc4f7f830ebb9ac95aae25138ffdc
youpiyoful <yoanfornari@gmail.com>
2020-01-18 00:07:17 +0100

    debug false

--- disquaire/settings.py
+++ disquaire/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*kw($h^_os^+xx4(tj%r5dvydri^z7&pe_rg!zw6qv#0%zd!ml'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
